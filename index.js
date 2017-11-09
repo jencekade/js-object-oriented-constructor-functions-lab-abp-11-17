5a9ea3e0be452f8c6710beb420225dc154b4227e
jencekade <gaephuoc@gmail.com>
2017-11-09 18:31:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function Scooter (year, color, model){
-  this[year] = year;
-  this[color] = color;
-  this[model] = model;
+function Scooter (year, color1, model){
+  this.year = year;
+  this.color1 = color1;
+  this.model = model;
 }
 
 function Driver (name, age, experience){
-  
+
 }
 
 function PickupLocation (address, city){
-  
+
 }
